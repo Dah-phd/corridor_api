92082f56344252c44289dbca2367315e6ff092b2
Daniel Nikolaev <toolsproed@gmail.com>
2022-06-04 16:37:26 +0300

    progress on AI quorid

--- src/quoridor/cpu.rs
+++ src/quoridor/cpu.rs
@@ -1,7 +1,9 @@
 use super::*;
+use std::collections::HashMap;
 const CPU: &str = "|CPU|";
 
 pub struct CpuPlayer {
+    difficulty: f32,
     game: Quoridor,
     cpu_path: Vec<(usize, usize)>,
     player_path: Vec<(usize, usize)>,
@@ -18,10 +20,15 @@ impl CpuPlayer {
                 return PlayerMove::QuoridorMove(new_position, CPU.to_owned());
             }
         }
-        todo!()
+        let maybe_wall = instance.get_best_wall();
+        if maybe_wall.is_some() {
+            return maybe_wall.unwrap();
+        }
+        return PlayerMove::QuoridorMove(instance.cpu_path.pop().unwrap(), CPU.to_owned());
     }
     fn new(game: Quoridor) -> Self {
         Self {
+            difficulty: 1.0,
             cpu_path: game.get_shortest_path(game.down_player, 0).unwrap(),
             player_path: game.get_shortest_path(game.up_player, 8).unwrap(),
             game,
@@ -44,15 +51,43 @@ impl CpuPlayer {
         false
     }
 
-    fn new_wall_path_len(&mut self, position: (usize, usize)) -> Option<usize> {
+    fn get_best_wall(&mut self) -> Option<PlayerMove> {
+        let mut path_results_h: Vec<(usize, (usize, usize))> = Vec::new();
+        let mut path_results_v: Vec<(usize, (usize, usize))> = Vec::new();
+        for idx in 0..self.player_path.len() - 1 {
+            self.add_new_hwall_path_result(self.player_path[idx], &mut path_results_h);
+            self.add_new_hwall_path_result(self.player_path[idx], &mut path_results_v);
+        }
+        let max_h = path_results_h.iter().max_by(|a, b| a.cmp(&b));
+        let max_v = path_results_v.iter().max_by(|a, b| a.cmp(&b));
+        if max_h.is_none() && max_v.is_none() {
+            return None;
+        }
+        if max_h.is_none() {
+            return Some(PlayerMove::QuoridorWallV(max_v.unwrap().1, CPU.to_owned()));
+        }
+        if max_v.is_none() {
+            return Some(PlayerMove::QuoridorWallH(max_h.unwrap().1, CPU.to_owned()));
+        }
+        let max_v = max_v.unwrap();
+        let max_h = max_h.unwrap();
+        if max_v.0 > max_h.0 {
+            return Some(PlayerMove::QuoridorWallV(max_v.1, CPU.to_owned()));
+        } else {
+            return Some(PlayerMove::QuoridorWallH(max_h.1, CPU.to_owned()));
+        }
+    }
+
+    fn add_new_hwall_path_result(&mut self, position: (usize, usize), storage: &mut Vec<(usize, (usize, usize))>) {
         if self.game.new_h_wall(position) {
             self.game.horizontal_walls.pop();
-            return Some(self.game.get_shortest_path(self.game.up_player, 0).unwrap().len());
+            storage.push((self.game.get_shortest_path(self.game.up_player, 0).unwrap().len(), position));
         }
+    }
+    fn add_new_vwall_path_result(&mut self, position: (usize, usize), storage: &mut Vec<(usize, (usize, usize))>) {
         if self.game.new_v_wall(position) {
             self.game.vertical_walls.pop();
-            return Some(self.game.get_shortest_path(self.game.up_player, 0).unwrap().len());
+            storage.push((self.game.get_shortest_path(self.game.up_player, 0).unwrap().len(), position));
         }
-        None
     }
 }
